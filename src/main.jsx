2a5947da0411aee1e3835c243aa8608a0685f24a
gehadmohame <88227666+gehadmohame@users.noreply.github.com>
2026-04-17 00:36:23 +0200

    remove server vercel config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-  return <h1 style={{ padding: 20 }}>APP IS WORKING 🚀</h1>;
-}
+import App from "./App.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
